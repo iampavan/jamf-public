126e1611c6ca79b1e7480da696dc092170ab51bc
iampavan <pavan.iwork@gmail.com>
2020-11-04 22:43:46 +0530

    Initial commit of EA - LDAP_search_for_country_name_based_on_user

---
+++ EA/LDAP_search_for_country_name_based_on_user/LDAP_search_for_country_name_based_on_user.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# This is an EA that performs an LDAP search based on the username that's assigned to the computer in Jamf Pro,
+# and the search can be filtered out based on any LDAP attribute.
+
+# EA name : Office Name (It can be called whatever.)
+
+# Variables
+jssURL="https://jamf.domain.com:8443/"
+apiUser="apiuser"
+apiPass="apipassword"
+
+LDAP_SERVER="ldap://domain.com"
+LDAP_USER="first.last@domain.com"
+LDAP_PASS="password_goes_here"
+
+SERIAL=$(ioreg -c IOPlatformExpertDevice -d 2 | awk -F\" '/IOPlatformSerialNumber/{print $(NF-1)}')
+# SERIAL="C02PRAJUVM02"
+
+USERINFO=$(curl -k ${jssURL}JSSResource/computers/serialnumber/${SERIAL}/subset/location -H "Accept: application/xml" --user "${apiUser}:${apiPass}")
+
+USERNAME=$(echo $USERINFO | /usr/bin/awk -F'<username>|</username>' '{print $2}' | tr [A-Z] [a-z])
+
+if [ -z "$USERNAME" ]; then # Return TRUE if $USERNAME variable is Empty.
+    RESULT="Username not found in Jamf"
+else
+    LDAP_QUERY=`ldapsearch -x -H $LDAP_SERVER -b 'DC=domain,DC=com' -D $LDAP_USER -w $LDAP_PASS "(sAMAccountName=$USERNAME)" "physicalDeliveryOfficeName" | grep physicalDeliveryOfficeName: | cut -d " " -f2-3`
+
+    if [ -z "$LDAP_QUERY" ]; then # Return TRUE if $LDAP_QUERY variable is Empty.
+        RESULT="Record not found in AD"
+    else
+        RESULT=$LDAP_QUERY
+    fi
+fi
+
+printf "<result>%s</result>" "${RESULT}"
