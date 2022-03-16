9c1e538982cd6ecafc881bcea5f72c19ea551215
Pavan Raju <pavan.iwork@gmail.com>
2022-03-16 17:00:28 +0530

    Sample_create_LaunchDaemon

---
+++ Scripts/Sample_create_LaunchDaemon/Create and Load LaunchDaemon.sh
@@ -0,0 +1,31 @@
+﻿#!/bin/sh
+
+cat << EOF > /Library/LaunchDaemons/com.pavan.mydaemon.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+   <key>Label</key>
+   <string>com.pavan.mydaemon</string>
+   <key>ProgramArguments</key>
+   <array>
+   	<string>/Library/Scripts/test.sh</string>
+   </array>
+   <key>RunAtLoad</key>
+   <true/>
+</dict>
+</plist>
+EOF
+
+/bin/cat << EOF > "/Library/Scripts/test.sh"
+#!/bin/bash
+"/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper" -windowType utility -description "Hello, World!" -button1 "OK" -button2 "Cancel"
+EOF
+
+# Make the test script executable
+chmod 755 /Library/Scripts/test.sh
+
+chmod 644 /Library/LaunchDaemons/com.pavan.mydaemon.plist
+chown root:wheel /Library/LaunchDaemons/com.pavan.mydaemon.plist
+
+/bin/launchctl load /Library/LaunchDaemons/com.pavan.mydaemon.plist
---
+++ Scripts/Sample_create_LaunchDaemon/Unload and remove LaunchDaemon.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/sh
+## unload and remove
+
+# Unload any launch daemons
+/bin/launchctl unload /Library/LaunchDaemons/com.pavan.mydaemon.plist
+
+# Remove any launch daemons
+/bin/rm /Library/LaunchDaemons/com.pavan.mydaemon.plist
+
+# Remove test script
+/bin/rm /Library/Scripts/test.sh
