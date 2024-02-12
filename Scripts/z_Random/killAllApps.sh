f05ca002add9098e09f85fc7026a0cc335ea72f2
Pavan Raju <praju16@bloomberg.net>
2024-02-12 11:12:32 +0000

    added description

--- Scripts/z_Random/killAllApps.sh
+++ Scripts/z_Random/killAllApps.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
-#Force quit all non-essential applications
+####################################################################################################
+#
+# THIS SCRIPT IS NOT AN OFFICIAL PRODUCT OF JAMF SOFTWARE
+# AS SUCH IT IS PROVIDED WITHOUT WARRANTY OR SUPPORT
+#
+# BY USING THIS SCRIPT, YOU AGREE THAT JAMF SOFTWARE
+# IS UNDER NO OBLIGATION TO SUPPORT, DEBUG, OR OTHERWISE
+# MAINTAIN THIS SCRIPT
+#
+####################################################################################################
+#
+# DESCRIPTION
+#
+# Force quit all non-essential applications
+# 
+####################################################################################################
+
 
 declare -a killPIDs
 killPIDs=$(ps axww -o pid,command | grep -v bash | grep [A]pplications/ | grep -v /bin/sh | grep -v [C]asper | grep -v [J]amf | grep -v [S]elf\ Service | grep -v [M]cAfee | grep -v [V]ZAccess* | grep -v grep | awk '{print $1}')
