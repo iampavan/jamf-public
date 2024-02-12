1724d7ac2dd42503726e0038e13830aceee31977
Pavan Raju <praju16@bloomberg.net>
2024-02-12 11:15:04 +0000

    removed blank line

--- Scripts/z_Random/killAllApps.sh
+++ Scripts/z_Random/killAllApps.sh
@@ -17,7 +17,6 @@
 # 
 ####################################################################################################
 
-
 declare -a killPIDs
 killPIDs=$(ps axww -o pid,command | grep -v bash | grep [A]pplications/ | grep -v /bin/sh | grep -v [C]asper | grep -v [J]amf | grep -v [S]elf\ Service | grep -v [M]cAfee | grep -v [V]ZAccess* | grep -v grep | awk '{print $1}')
 
